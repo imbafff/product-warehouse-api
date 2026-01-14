2b0d0048fa416d9a6266c30766860f853013af25
m1603498 <tomurasensoma@gmail.com>
2026-01-14 19:46:21 +0500

    feat: implement product service and HTTP API with infrastructure

---
+++ internal/usecase/product/repository.go
@@ -0,0 +1,11 @@
+package product
+
+import "github.com/imbafff/product-warehouse-api/internal/entity"
+
+type Repository interface {
+	Create(product *entity.Product) (int64, error)
+	GetByID(id int64) (*entity.Product, error)
+	Update(id int64, product *entity.Product) error
+	Delete(id int64) error
+	GetAll() ([]*entity.Product, error)
+}
